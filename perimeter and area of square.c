d66d32e597d48c140c53b4f16b44fe5799264530
hanisha2703 <makhijahanisha2703@gmail.com>
2025-08-12 16:49:42 +0530

    Add files via upload

---
+++ perimeter and area of square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter the value for length of square");
+    scanf("%d",&a);
+    b=a*a;
+    printf("area = %d units square\n",b);
+    scanf("%d",&a);
+    b=a*4;
+    printf("perimeter = %d units\n",b);
+}
